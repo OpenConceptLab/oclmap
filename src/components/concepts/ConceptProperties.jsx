49396bba72b7dad6a7176d5d40ed1b6ef7a00874
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-06-01 17:49:51 +0530

    Fixing concept boolean property representation

--- src/components/concepts/ConceptProperties.jsx
+++ src/components/concepts/ConceptProperties.jsx
@@ -48,7 +48,7 @@ const ConceptProperties = ({ concept }) => {
                   {definition.label}
                 </TableCell>
                 <TableCell style={{fontSize: '0.875rem'}} className='searchable'>
-                  {definition.value}
+                  {isBoolean(definition.value) ? definition.value.toString() : definition.value}
                 </TableCell>
               </TableRow>
             )
