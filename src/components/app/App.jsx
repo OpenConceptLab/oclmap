3ee7c436b8b5f0cb67774d14e71ba17cbe26dcf0
Sunny Aggarwal <sny.aggarwal@gmail.com>
2026-07-17 09:41:32 +0530

    Added community site redirection

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Route, Switch, withRouter } from 'react-router-dom';
 import {
-  recordGAPageView, isLoggedIn, getLoginURL, isV3URL, isV2URL, isRedirectingToLoginViaReferrer,
+  recordGAPageView, isLoggedIn, getLoginURL, isOtherOCLClientURL, isRedirectingToLoginViaReferrer,
   isInWaitlist, getEnv
 } from '../../common/utils';
 import Error404 from '../errors/Error404';
@@ -91,7 +91,7 @@ const App = props => {
     const referrer = queryParams.get('referrer')
     if(isLoggedIn()) {
       window.location.hash = '#'  + pathname
-    } else if(referrer && (isV3URL(referrer) || isV2URL(referrer)) && !isLoggedIn()) {
+    } else if(isOtherOCLClientURL(referrer) && !isLoggedIn()) {
       const parts = hash.split('?')
       let params = new URLSearchParams(parts[1])
       if(params.get('auth') === 'true') {
